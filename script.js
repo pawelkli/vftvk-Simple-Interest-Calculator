e73e65946f5a4fb7bd3572669e1029ebb543c255
pawelkli <klimowicz.pawel@gmail.com>
2021-12-20 21:36:17 +0000

    backgroud a little yellowish

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function compute()
     var years = document.getElementById("years").value;
     var interest = principal * years * rate /100;
     var year = new Date().getFullYear()+parseInt(years);
-    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>"
+    document.getElementById("result").innerHTML="If you deposit <span class=\"yellow\">"+principal+"</span>,\<br\>at an interest rate of <span class=\"yellow\">"+rate+"%</span>\<br\>You will receive an amount of <span class=\"yellow\">"+interest+"</span>,\<br\>in the year <span class=\"yellow\">"+year+"</span>\<br\>"
 }
         
 function updateRate() 
